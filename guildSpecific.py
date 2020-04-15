4e5f17808beb986919945ddeabd3eaf6dc172c35
Lucas <lucaswfreeman@gmail.com>
2020-04-15 14:58:18 -0500

    creating displayMap

--- guildSpecific.py
+++ guildSpecific.py
@@ -1,17 +1,26 @@
 import discord
 from discord.ext import commands
-# this can be used to define channel specific
+from miscFunctions import miscFunctions
+# Example of how to build out the fucntions in this file:
+    # class guildSpecific(commands.Cog):
+    #     def __init__(self,bot):
+    #         self.bot = bot
+    #         self.guildId= 0 #replace 0 with the guild ID
 
-class guildSpecific(commands.Cog):
+    #     @commands.command()
+    #     async def guildSpecificFunction(self,ctx):
+    #         if ctx.guild.id == self.guildId:
+    #             pass #logic here
+# You can add as many classes you want that will be guild specific. Be sure to include them into the "setup" function or the bot won't use it.
+class dsNDs(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-        self.guildId= 0 #replace 0 with the guild ID
+        self.guildId= 658080668886237194 #replace 0 with the guild ID
 
     @commands.command()
-    async def guildSpecificFunction(self,ctx):
+    async def displayMap(self,ctx):
         if ctx.guild.id == self.guildId:
-            pass  #logic here
-
+            await miscFunctions.displayImage(ctx,"swordCoast.png",messageText="Here is your map: ")
 
 def setup(bot):
-    bot.add_cog(guildSpecific(bot))
+    bot.add_cog(dsNDs(bot))
--- miscFunctions.py
+++ miscFunctions.py
@@ -3,11 +3,10 @@ from discord.ext import commands
 
 class miscFunctions():
 
-    # @commands.Cog.listener()
-    # async def on_ready(self):
-        # await def on_raw_reaction_add(self,payload):
-        #     if payload.emoji.id == 697266516709605447 and payload.user_id == 158058710760030219:
-        #         message = await self.bot.fetch_message(payload.message_id)
-
-    async def displayImage(self,ctx,imagePath):
-        ctx.send(file="swordCoast.png")
+    @classmethod
+    async def displayImage(self,ctx,imagePath,messageText=None):
+        # await ctx.send(file=discord.file())
+        if messageText is not None:
+            await ctx.send(str(messageText),file = discord.File(imagePath))
+        else:
+            await ctx.send(file = discord.File(imagePath))
